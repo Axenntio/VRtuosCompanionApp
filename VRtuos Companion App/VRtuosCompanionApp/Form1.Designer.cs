9a21113ad8279ba75e157b1abe66ae5809249b27
KureKureciCZ <48494136+KureKureciCZ@users.noreply.github.com>
2020-06-06 19:41:30 +0200

    Add files via upload

---
+++ VRtuos Companion App/VRtuosCompanionApp/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace VRtuosCompanionApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.OutputCombo = new System.Windows.Forms.ComboBox();
+            this.Button = new System.Windows.Forms.Button();
+            this.RefreshButton = new System.Windows.Forms.Button();
+            this.ConnectionStatusLabel = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.IPTextbox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.InputCombo = new System.Windows.Forms.ComboBox();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Georgia", 55F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(42, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(831, 85);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "VRtuos Companion App";
+            // 
+            // OutputCombo
+            // 
+            this.OutputCombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.OutputCombo.Font = new System.Drawing.Font("Arial", 14F);
+            this.OutputCombo.FormattingEnabled = true;
+            this.OutputCombo.Location = new System.Drawing.Point(57, 267);
+            this.OutputCombo.Name = "OutputCombo";
+            this.OutputCombo.Size = new System.Drawing.Size(349, 30);
+            this.OutputCombo.TabIndex = 9;
+            this.OutputCombo.SelectedIndexChanged += new System.EventHandler(this.OutputCombo_SelectedIndexChanged);
+            // 
+            // Button
+            // 
+            this.Button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.Button.FlatAppearance.BorderSize = 0;
+            this.Button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Button.Font = new System.Drawing.Font("Arial", 14F);
+            this.Button.Location = new System.Drawing.Point(671, 245);
+            this.Button.Name = "Button";
+            this.Button.Size = new System.Drawing.Size(172, 30);
+            this.Button.TabIndex = 5;
+            this.Button.Text = "Connect";
+            this.Button.UseVisualStyleBackColor = false;
+            this.Button.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // RefreshButton
+            // 
+            this.RefreshButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.RefreshButton.FlatAppearance.BorderSize = 0;
+            this.RefreshButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.RefreshButton.Font = new System.Drawing.Font("Arial", 14F);
+            this.RefreshButton.Location = new System.Drawing.Point(234, 303);
+            this.RefreshButton.Name = "RefreshButton";
+            this.RefreshButton.Size = new System.Drawing.Size(172, 30);
+            this.RefreshButton.TabIndex = 10;
+            this.RefreshButton.Text = "Refresh";
+            this.RefreshButton.UseVisualStyleBackColor = false;
+            this.RefreshButton.Click += new System.EventHandler(this.RefreshButton_Click);
+            // 
+            // ConnectionStatusLabel
+            // 
+            this.ConnectionStatusLabel.AutoSize = true;
+            this.ConnectionStatusLabel.BackColor = System.Drawing.Color.Transparent;
+            this.ConnectionStatusLabel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ConnectionStatusLabel.Font = new System.Drawing.Font("Arial", 14F);
+            this.ConnectionStatusLabel.ForeColor = System.Drawing.Color.White;
+            this.ConnectionStatusLabel.Location = new System.Drawing.Point(650, 278);
+            this.ConnectionStatusLabel.Name = "ConnectionStatusLabel";
+            this.ConnectionStatusLabel.Size = new System.Drawing.Size(133, 22);
+            this.ConnectionStatusLabel.TabIndex = 6;
+            this.ConnectionStatusLabel.Text = "Not connected";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label5.Font = new System.Drawing.Font("Arial", 14F);
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(53, 184);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(118, 22);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Input device:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label3.Font = new System.Drawing.Font("Arial", 14F);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(490, 278);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(166, 22);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Connection status:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label6.Font = new System.Drawing.Font("Arial", 14F);
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(53, 242);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(132, 22);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Output device:";
+            // 
+            // IPTextbox
+            // 
+            this.IPTextbox.Font = new System.Drawing.Font("Arial", 14F);
+            this.IPTextbox.Location = new System.Drawing.Point(494, 210);
+            this.IPTextbox.MaxLength = 15;
+            this.IPTextbox.Name = "IPTextbox";
+            this.IPTextbox.Size = new System.Drawing.Size(349, 29);
+            this.IPTextbox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Font = new System.Drawing.Font("Arial", 14F);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(490, 185);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 22);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "IP address:";
+            // 
+            // InputCombo
+            // 
+            this.InputCombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.InputCombo.Font = new System.Drawing.Font("Arial", 14F);
+            this.InputCombo.FormattingEnabled = true;
+            this.InputCombo.Location = new System.Drawing.Point(57, 209);
+            this.InputCombo.Name = "InputCombo";
+            this.InputCombo.Size = new System.Drawing.Size(349, 30);
+            this.InputCombo.TabIndex = 8;
+            this.InputCombo.SelectedIndexChanged += new System.EventHandler(this.InputCombo_SelectedIndexChanged);
+            // 
+            // Timer
+            // 
+            this.Timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label4.Font = new System.Drawing.Font("Arial", 10F);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(491, 131);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(292, 32);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Make sure VRtuos is running and your Quest\r\nis connected to the same network.";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.Button;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.ClientSize = new System.Drawing.Size(914, 442);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.RefreshButton);
+            this.Controls.Add(this.OutputCombo);
+            this.Controls.Add(this.InputCombo);
+            this.Controls.Add(this.Button);
+            this.Controls.Add(this.ConnectionStatusLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.IPTextbox);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "VRtuos Companion App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Leave += new System.EventHandler(this.Form1_Leave);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox OutputCombo;
+        private System.Windows.Forms.Button Button;
+        private System.Windows.Forms.Button RefreshButton;
+        private System.Windows.Forms.Label ConnectionStatusLabel;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox IPTextbox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox InputCombo;
+        private System.Windows.Forms.Timer Timer;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ VRtuos Companion App/VRtuosCompanionApp/Form1.cs
@@ -0,0 +1,339 @@
+﻿using System;
+using System.Drawing;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using Melanchall.DryWetMidi.Core;
+using Melanchall.DryWetMidi.Devices;
+
+namespace VRtuosCompanionApp
+{
+    public partial class Form1 : Form
+    {
+        private static InputDevice inputDevice;
+        private static OutputDevice outputDevice;
+
+        static UdpClient udpServer;
+        static UdpClient udpClient;
+        static Thread thdUDPServer;
+
+        private static ConnectionStatus connectionStatus = ConnectionStatus.Disconnected;
+        static string address;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            Connect();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RefreshDevices();
+
+            //Run the server thread and create a client
+            udpServer = new UdpClient(28008);
+            thdUDPServer = new Thread(new ThreadStart(serverThread));
+            thdUDPServer.IsBackground = true;
+            thdUDPServer.Start();
+        }
+
+        private void Form1_Leave(object sender, EventArgs e)
+        {
+            inputDevice.Dispose();
+            outputDevice.Dispose();
+
+            Disconnect();
+            if (udpServer != null)
+                udpServer.Close();
+            if (thdUDPServer != null)
+            {
+                thdUDPServer.Join(2000);
+                thdUDPServer.Abort();
+            }
+        }
+
+        private void RefreshButton_Click(object sender, EventArgs e)
+        {
+            RefreshDevices();
+        }
+
+        private void Connect(bool isAutomatic = false)
+        {
+            //Decides if it should fill in the ip address,
+            //or if it should set it to the one in the textbox
+            if (!isAutomatic)
+            {
+                Disconnect();
+                address = IPTextbox.Text;
+            }
+            else
+            {
+                IPTextbox.Text = address;
+            }
+
+
+            if (IPAddress.TryParse(address, out IPAddress iPAddress))
+            {
+                udpClient = new UdpClient();
+
+                try
+                {
+                    udpClient.Connect(iPAddress, 28008);
+                    //Sends answer to the server and asks for a reaction
+                    //OR just asks for a reaction
+                    if (isAutomatic)
+                    {
+                        SendData(Encoding.ASCII.GetBytes("YesThereIs"), (int)code.connectionData);
+                        SendData(Encoding.ASCII.GetBytes("IsAnybodyThere"), (int)code.connectionData);
+                    }
+                    else
+                        SendData(Encoding.ASCII.GetBytes("IsAnybodyThere"), (int)code.connectionData);
+
+                }
+                catch (Exception)
+                {
+                    ConnectionStatusLabel.ForeColor = Color.Red;
+                    ConnectionStatusLabel.Text = "Error";
+                    return;
+                }
+                connectionStatus = ConnectionStatus.Connecting;
+                Timer.Enabled = true;
+            }
+            else
+            {
+                ConnectionStatusLabel.ForeColor = Color.Red;
+                ConnectionStatusLabel.Text = "Incorrect IP address";
+            }
+        }
+
+        private void Disconnect()
+        {
+            if (udpClient != null)
+                udpClient.Close();
+            connectionStatus = ConnectionStatus.Disconnected;
+        }
+
+        private void RefreshDevices()
+        {
+            InputCombo.Items.Clear();
+            foreach (InputDevice device in InputDevice.GetAll())
+            {
+                InputCombo.Items.Add(device.Name);
+            }
+            InputCombo.SelectedIndex = InputCombo.Items.Count - 1;
+
+            OutputCombo.Items.Clear();
+            foreach (OutputDevice device in OutputDevice.GetAll())
+            {
+                OutputCombo.Items.Add(device.Name);
+            }
+            OutputCombo.SelectedIndex = OutputCombo.Items.Count - 1;
+        }
+
+        private static void OnMidiRecieved(object sender, MidiEventReceivedEventArgs e)
+        {
+            if (e.Event != null)
+            {
+                if (e.Event.EventType == MidiEventType.NoteOn ||
+                    e.Event.EventType == MidiEventType.NoteOff)
+                {
+                    NoteEvent noteEvent = e.Event as NoteEvent;
+                    SendMidi(noteEvent);
+                }
+                else if (e.Event.EventType == MidiEventType.ControlChange)
+                {
+                    ControlChangeEvent ccEvent = e.Event as ControlChangeEvent;
+                    SendMidi(ccEvent);
+                }
+                else if (e.Event.EventType == MidiEventType.ProgramChange)
+                {
+                    ProgramChangeEvent pcEvent = e.Event as ProgramChangeEvent;
+                    SendMidi(pcEvent);
+                }
+            }
+        }
+
+
+        private static void SendMidi(ControlChangeEvent controlChangeEvent)
+        {
+            if (connectionStatus == ConnectionStatus.Connected)
+            {
+                MidiEventToBytesConverter converter = new MidiEventToBytesConverter();
+                SendData(converter.Convert(controlChangeEvent), (int)code.midi);
+            }
+        }
+
+        private static void SendMidi(NoteEvent noteEvent)
+        {
+            if (connectionStatus == ConnectionStatus.Connected)
+            {
+                MidiEventToBytesConverter converter = new MidiEventToBytesConverter();
+                SendData(converter.Convert(noteEvent), (int)code.midi);
+            }
+        }
+
+        private static void SendMidi(ProgramChangeEvent programChangeEvent)
+        {
+            if (connectionStatus == ConnectionStatus.Connected)
+            {
+                MidiEventToBytesConverter converter = new MidiEventToBytesConverter();
+                SendData(converter.Convert(programChangeEvent), (int)code.midi);
+            }
+        }
+
+
+        private void InputCombo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (inputDevice != null)
+            {
+                inputDevice.StopEventsListening();
+                inputDevice.Dispose();
+            }
+
+            inputDevice = InputDevice.GetByName(InputCombo.SelectedItem.ToString());
+            inputDevice.EventReceived += new EventHandler<MidiEventReceivedEventArgs>(Form1.OnMidiRecieved);
+
+            if (!inputDevice.IsListeningForEvents)
+                inputDevice.StartEventsListening();
+        }
+
+        private void OutputCombo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (outputDevice != null)
+            {
+                outputDevice.Dispose();
+            }
+            outputDevice = OutputDevice.GetByName(OutputCombo.SelectedItem.ToString());
+        }
+
+        public static void PlayMidi(MidiEvent noteEvent)
+        {
+            if (outputDevice != null)
+                outputDevice.SendEvent(noteEvent);
+        }
+
+
+        private static void SendData(byte[] data, int code)
+        {
+            //Make the first byte an int code.
+            //This will let the other end decide what to do with the data quicker
+            byte[] toSend = new byte[data.Length + 1];
+            toSend[0] = (byte)code;
+            data.CopyTo(toSend, 1);
+
+            udpClient.Send(toSend, toSend.Length);
+        }
+
+        private void serverThread()
+        {
+            while (true)
+            {
+                try
+                {
+                    //Get the other IP, read data
+                    IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+                    Byte[] receiveBytes = udpServer.Receive(ref RemoteIpEndPoint);
+                    byte[] data = new byte[receiveBytes.Length - 1];
+                    Array.Copy(receiveBytes, 1, data, 0, receiveBytes.Length - 1);
+                    string returnData = Encoding.ASCII.GetString(data);
+
+                    Console.WriteLine(RemoteIpEndPoint.Address.ToString()
+                                            + ":" + returnData.ToString());
+
+                    switch (receiveBytes[0])
+                    {
+                        case 10:
+                            //If midi, play it
+                            BytesToMidiEventConverter converter = new BytesToMidiEventConverter();
+                            MidiEvent tmpevent = converter.Convert(data);
+                            PlayMidi(tmpevent);
+                            break;
+
+                        case 20:
+                            //Reacts
+                            if (returnData.ToString() == "IsAnybodyThere")
+                            {
+                                if (connectionStatus != ConnectionStatus.Connected && connectionStatus != ConnectionStatus.Connecting)
+                                {
+                                    address = RemoteIpEndPoint.Address.ToString();
+
+                                    MethodInvoker inv = delegate
+                                    {
+                                        Connect(true);
+                                    };
+                                    Invoke(inv);
+                                }
+                                else
+                                {
+                                    //Answers
+                                    SendData(Encoding.ASCII.GetBytes("YesThereIs"), (int)code.connectionData);
+                                }
+                            }
+                            else if (returnData.ToString() == "YesThereIs")
+                            {
+                                //Connection is established
+                                connectionStatus = ConnectionStatus.Connected;
+                            }
+                            break;
+
+                        default:
+                            break;
+                    }
+                }
+                catch (Exception)
+                {
+                    connectionStatus = ConnectionStatus.Disconnected;
+                    break;
+                }
+            }
+        }
+
+        int timeout = 50;
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            //Just for visuals
+            switch (connectionStatus)
+            {
+                case ConnectionStatus.Connecting:
+                    ConnectionStatusLabel.ForeColor = Color.Yellow;
+                    ConnectionStatusLabel.Text = "Connecting...";
+                    timeout--;
+                    if (timeout <= 0)
+                        Disconnect();
+                    break;
+
+                case ConnectionStatus.Connected:
+                    ConnectionStatusLabel.ForeColor = Color.Green;
+                    ConnectionStatusLabel.Text = "Connected";
+                    Timer.Enabled = false;
+                    timeout = 50;
+                    break;
+
+                case ConnectionStatus.Disconnected:
+                    ConnectionStatusLabel.ForeColor = Color.Red;
+                    ConnectionStatusLabel.Text = "Disconnected";
+                    timeout = 50;
+                    break;
+            }
+        }
+    }
+
+    enum ConnectionStatus
+    {
+        Connecting,
+        Connected,
+        Disconnected,
+    }
+
+    enum code
+    {
+        midi = 10,
+        connectionData = 20
+    }
+}
